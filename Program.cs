24be5927f8e77356d832810ddd803e65bb03f157
Roman Lovchikov <a-s_roman@mail.ru>
2025-01-25 11:21:58 +0300

    fix scales

--- Program.cs
+++ Program.cs
@@ -8,11 +8,16 @@ namespace RowyRandomizer
 	{
 		public static void Main(string[] args)
 		{
+			byte massiveWeaponsMinDelay = args.Length > 0 ? byte.Parse(args[0]) : (byte)25;
+			Randomize(massiveWeaponsMinDelay);
+		}
+
+		private static void Randomize(byte massiveWeaponsMinDelay)
+		{
 			byte _weaponsCount = 55;
 			HashSet<WeaponDelayIndex> _massiveWeapons = new HashSet<WeaponDelayIndex> { WeaponDelayIndex.EQuake, WeaponDelayIndex.Nuclear, WeaponDelayIndex.Arma };
 			
-			byte _massiveWeaponsMinDelay = args.Length > 0 ? byte.Parse(args[0]) : (byte)25;
-			if (_massiveWeaponsMinDelay > _weaponsCount - _massiveWeapons.Count) _massiveWeaponsMinDelay = (byte)(_weaponsCount - _massiveWeapons.Count);
+			if (massiveWeaponsMinDelay > _weaponsCount - _massiveWeapons.Count) massiveWeaponsMinDelay = (byte)(_weaponsCount - _massiveWeapons.Count);
 			
 		    string root = Directory.GetCurrentDirectory();
 		    string pathIn = Path.Combine(root, "RowyDefault.wsc");
@@ -27,14 +32,14 @@ namespace RowyRandomizer
 		    List<byte> indexPool_Massive = new List<byte>();
 		    List<byte> indexPool_All = new List<byte>();
 
-		    if (_massiveWeaponsMinDelay > 0)
-			    FillPool(_massiveWeaponsMinDelay, _weaponsCount, ref indexPool_Massive);
-		    FillPool(0, _massiveWeaponsMinDelay > 0 ? _massiveWeaponsMinDelay : _weaponsCount, ref indexPool_All);
+		    if (massiveWeaponsMinDelay > 0)
+			    FillPool(massiveWeaponsMinDelay, _weaponsCount, ref indexPool_Massive);
+		    FillPool(0, massiveWeaponsMinDelay > 0 ? massiveWeaponsMinDelay : _weaponsCount, ref indexPool_All);
 
 		    Random randomizer = new Random();
 			
 		    //massive weapons first
-		    if (_massiveWeaponsMinDelay > 0)
+		    if (massiveWeaponsMinDelay > 0)
 		    {
 			    foreach (WeaponDelayIndex massiveWeapon in _massiveWeapons)
 			    {
@@ -48,7 +53,7 @@ namespace RowyRandomizer
 		    //rest of weapons
 		    foreach (WeaponDelayIndex weaponDelayIndex in Enum.GetValues(typeof(WeaponDelayIndex)))
 		    {
-			    if (_massiveWeaponsMinDelay > 0 && _massiveWeapons.Contains(weaponDelayIndex)) continue;
+			    if (massiveWeaponsMinDelay > 0 && _massiveWeapons.Contains(weaponDelayIndex)) continue;
 			    int random = randomizer.Next(0, indexPool_All.Count);
 			    byte value = indexPool_All[random];
 			    schemeData[(int)weaponDelayIndex] = value;
@@ -59,7 +64,7 @@ namespace RowyRandomizer
 		    writer.Write(schemeData);
 		    writer.Close();
 		}
-		
+
 		private static void FillPool(byte min, byte max, ref List<byte> pool)
 		{
 			for (byte i = min; i < max; i++) pool.Add(i);
@@ -105,12 +110,12 @@ namespace RowyRandomizer
 			OldWoman = 183,
 			SheepLauncher = 187,
 			SuperSheep = 191,
-			MoleBomb = 195,
+			MoleBomb = 195,//39
 			SuperBanana = 227,
 			MineStrike = 231,
 			GirderPack = 235,
 			EQuake = 239,
-			Scales = 244,
+			Scales = 243,
 			MingVase = 247,
 			CarpetBomb = 251,
 			Nuclear = 259,
